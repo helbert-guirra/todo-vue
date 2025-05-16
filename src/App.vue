69258de29c8bd0db221c19faeed564b8340ea2db
HelbertGuirra <helbert.guirra@gmail.com>
2025-05-16 20:10:43 -0300

    corrigindo erro do cabecalho

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
   import { reactive } from 'vue';
-  import cabecalho from './components/cabecalho.vue'
+  import Cabecalho from './components/cabecalho.vue'
   import Formulario from './components/formulario.vue';
   import ListaDeTarefas from './components/ListaDeTarefas.vue';
 
